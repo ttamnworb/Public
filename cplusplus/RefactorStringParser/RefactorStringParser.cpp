74403633664e2aec79d7053c2903393c208086c8
ttamnworb <22097106+ttamnworb@users.noreply.github.com>
2024-02-23 15:01:49 +0000

    Update RefactorStringParser.cpp

--- cplusplus/RefactorStringParser/RefactorStringParser.cpp
+++ cplusplus/RefactorStringParser/RefactorStringParser.cpp
@@ -1,19 +1,31 @@
-// RefactorStringParser.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 #include <string>
 #include <vector>
 #include <iostream>
 #include <regex>
 #include <algorithm>
 
+
+// Explanation.
+//	At work someone presented me with (something similar to) the following code to review.
+//  When I looked at the code I saw that it was a hand coded, long way of doing the task and 
+//  suggested the alternative.
+//  I failed to convince the original author that my version would be better, because I didn't
+//  explain why using std::lib functions was preferable in most cases to hand crafted ones.
+//  I agree that my code is not trivial and it could have done with additional comments, lesson learned.
+
+
+
+/// <summary>
 /// The original code.
 /// Purpose:
-///			Accept a string containing semi colon seperated numbers
+///			Accept a string containing semi colon separated numbers
 ///			Spaces will be ignored either side of the semicolon
 ///			All numbers must be between 1 and 65535
-///			'Numbers' comtaining invalid characters will not be parsed.
+///			'Numbers' containing invalid characters will not be parsed.
 ///			eg:	12345;11111;12121;22222;11QT2
-/// 
+/// </summary>
+/// <param name="strInput">A semi colon separated list of numbers</param>
+/// <returns>return the list of valid numbers from the input</returns>
 std::vector<int> Original_ParseString(const std::string& input)
 {
 	std::vector<int> results;
@@ -55,15 +67,44 @@ std::vector<int> Original_ParseString(const std::string& input)
 	return results;
 }
 
-
+/// <summary>
+/// This is my suggested alternative version of the function
+/// </summary>
+/// <param name="strInput">A semi colon separated list of numbers</param>
+/// <returns>return the list of valid numbers from the input</returns>
 std::vector<int> New_ParseString(const std::string& strInput)
 {
-	const std::regex re("/d*/d*/d*/d*/d*/s*;*/s*");
+	//const std::regex re("/d*/d*/d*/d*/d*/s*;*/s*");
+	const std::regex re("\\d*\\d*\\d*\\d*\\d*[\\s;]*");
+//	std::vector<int> result;
+
+	std::cout << strInput << "\n";
+	
+	auto words_begin = std::sregex_iterator(strInput.begin(), strInput.end(), re);
+	auto words_end = std::sregex_iterator();
+
+	std::cout << "Found " << std::distance(words_begin, words_end) << " words:\n";
+
+	for (std::sregex_iterator i = words_begin; i != words_end; ++i)
+	{
+		std::smatch match = *i;
+		std::string match_str = match.str();
+		std::cout << match_str << '\n';
+	}
+
+
+
+
+
+
+	std::vector<int> result;
 	std::vector<std::string> output;
 	std::copy_if(std::sregex_token_iterator(strInput.cbegin(), strInput.end(), re, -1), std::sregex_token_iterator(),
-		std::back_inserter(output),
+		//std::back_inserter(output),
+		std::ostream_iterator<std::string>(std::cout, " "),
 		[](std::string const& s) {return s.size() != 0; });
-
+	
+	return result;
 }
 
 
@@ -72,7 +113,7 @@ std::vector<int> New_ParseString(const std::string& strInput)
 /// </summary>
 /// <param name="header"></param>
 /// <param name="results"></param>
-void DumpResults(const std::string& header, const std::vector<int>& results)
+void PrintResults(const std::string& header, const std::vector<int>& results)
 {
 	std::cout << header.c_str() << "\n";
 	for (auto iResult = results.begin(); iResult != results.end(); ++iResult)
@@ -90,15 +131,15 @@ int main()
 {
     std::vector<std::string> testData =
     {
-        "12345;11111;12121;22222;11QT2"
+        "12345;11111;12121;22222;11QT2;444"
     };
 
 	
 
     for (auto iTestData = testData.begin(); iTestData != testData.end(); ++iTestData)
     {
-       DumpResults ("Original", Original_ParseString(*iTestData));
-	   DumpResults ("Refactored", New_ParseString(*iTestData));
+       //PrintResults ("Original", Original_ParseString(*iTestData));
+	   PrintResults ("Refactored", New_ParseString(*iTestData));
     }
 
 }
